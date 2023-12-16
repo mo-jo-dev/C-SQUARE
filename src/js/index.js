7c0f959293a5e0e38e1066957599d813e34dc75e
Mohit Joshi <b210055@nitsikkim.ac.in>
2023-12-17 00:09:51 +0530

    Added the files and wrote api call for data

--- src/js/index.js
+++ src/js/index.js
@@ -1,13 +1,14 @@
-async function getData(){
-    try {
-        const res = await fetch("https://api.coronavirus.data.gov.uk/v1/data");
-        const result = await res.findAll({
-            where: {
-                phase: "BEFORE"
-            }
-        })
-    } catch (error) {
-        console.log("Not able to fetch the details of contest");
+    async function getData(){
+        try {
+            const res = await fetch("https://api.coronavirus.data.gov.uk/v1/data");
+            const result = await res.findAll({
+                where: {
+                    phase: "BEFORE"
+                }
+            });
+            return result;
+        } catch (error) {
+            console.log("Not able to fetch the details of contest");
         return res.status(404).json({
             message: 'Not able to fetch',
         });
