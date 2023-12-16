1b654983ad35be069fcfcd17c3cac5697341343b
Mohit Joshi <b210055@nitsikkim.ac.in>
2023-12-17 00:08:52 +0530

    Added the files and wrote api call for data

---
+++ src/js/index.js
@@ -0,0 +1,15 @@
+async function getData(){
+    try {
+        const res = await fetch("https://api.coronavirus.data.gov.uk/v1/data");
+        const result = await res.findAll({
+            where: {
+                phase: "BEFORE"
+            }
+        })
+    } catch (error) {
+        console.log("Not able to fetch the details of contest");
+        return res.status(404).json({
+            message: 'Not able to fetch',
+        });
+    }
+}
